97f46f2eadde4fef0037bf92de7b76fe38769c0b
Ryan Kan <25820201+Ryan-Kan@users.noreply.github.com>
2022-08-09 13:25:14 +0800

    Add Helper Python File To Create Upload ZIP

---
+++ Package Contents Into ZIP.py
@@ -0,0 +1,37 @@
+# IMPORTS
+import zipfile
+
+from git import Repo
+
+# CONSTANTS
+FILES_TO_ADD = [
+    ".gitattributes",
+    ".gitignore",
+    "application.py",
+    "Procfile",
+    "requirements.txt",
+    "wsgi.py",
+]
+
+
+# HELPER FUNCTIONS
+def get_latest_commit_timestamp():
+    repo = Repo(".")
+    latest_commit = list(repo.iter_commits("main", max_count=1))[0]
+    return int(latest_commit.committed_datetime.timestamp())
+
+
+# MAIN CODE
+if __name__ == "__main__":
+    # Get latest commit timestamp
+    latestTimestamp = get_latest_commit_timestamp()
+
+    # Create the ZIP file
+    name = f"Upload-{latestTimestamp}.zip"
+    with zipfile.ZipFile(name, "w") as f:
+        # Add required files
+        for file in FILES_TO_ADD:
+            f.write(file)
+
+    # Report completion
+    print(f"Created '{name}' using commit with timestamp {latestTimestamp}.")
